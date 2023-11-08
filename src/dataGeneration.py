8ec5b21b08722ede732c2dff34fcffc8d6561c1f
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2023-11-08 09:28:12 +0100

    generating data

--- src/dataGeneration.py
+++ src/dataGeneration.py
@@ -1,11 +1,10 @@
+### This file is used to generate data for standard and actual routes ###
+
 import json
 import random
+import routeGenerator
 
-# for testing purposes
-# nb_cities = len(cities)
-# nb_merchandise_types = len(merchandise_types)
-
-## Generate our data for actual and standard routes
+## getting the list of cities, merchandise types and drivers
 
 # read and parse cities.json
 with open("./data/cities.json", "r") as cities_file:
@@ -15,6 +14,38 @@ with open("./data/cities.json", "r") as cities_file:
 with open("./data/merchTypes.json", "r") as merchTypes_file:
     merchandise_types = json.load(merchTypes_file)
 
+# read and parse drivers.json
+with open("./data/drivers.json", "r") as drivers_file:
+    drivers = json.load(drivers_file)
+
+
+## generate standard.json
+nb_std_routes = 100 # define the number of standard routes
+standard_routes = []
+for i in range(nb_std_routes):
+    standard_routes.append({
+        "id": f"s{i + 1}",
+        "route": routeGenerator.generate_route(cities, merchandise_types)
+    })
+
+# save data into standard.json
+with open("./data/standard.json", "w") as standard_file:
+    json.dump(standard_routes, standard_file, indent=4)
+
+## generate actual.json
+nb_act_routes = 500 # define the number of actual routes
+actual_routes = []
+for i in range(nb_act_routes):
+    actual_routes.append({
+        "id": f"a{i + 1}",
+        "driver": random.choice(drivers),
+        "sroute": f"s{random.randint(1, nb_std_routes)}",
+        "route": routeGenerator.generate_route(cities, merchandise_types)
+    })
+
+# save data into actual.json
+with open("./data/actual.json", "w") as actual_file:
+    json.dump(actual_routes, actual_file, indent=4)
 
 
 
