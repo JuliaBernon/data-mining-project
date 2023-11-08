38edb26f51e5d91c1bfa9fa82ce2af5d3da669cf
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2023-11-08 09:28:32 +0100

    function to generate routes

---
+++ src/routeGenerator.py
@@ -0,0 +1,25 @@
+### This file contains a function to generate a route, given a set of cities and merchandise types ###
+
+import random
+
+# function to generate a route
+def generate_route(cities, merchandise_types):
+
+    '''
+    Generate a route, given a set of cities and merchandise types
+    '''
+
+    std_route = []
+    from_city = random.choice(cities)
+    to_city = random.choice(cities)
+    while from_city == to_city:
+        to_city = random.choice(cities)
+    
+    num_trips = random.randint(1, 5)  # number of trips (1 to 5)
+    for _ in range(num_trips):
+        # random number of merchandise types with maximum 50 items of each type
+        merchandise = {item: random.randint(1, 50) for item in random.sample(merchandise_types, random.randint(1, len(merchandise_types)))}
+        std_route.append({"from": from_city, "to": to_city, "merchandise": merchandise})
+        from_city = to_city
+        to_city = random.choice(cities)
+    return std_route
