47c8d27bd4d6a462bf02ae0c890b3151214401d4
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2023-11-09 19:23:42 +0100

    Transform a route into a matrix

--- src/data2Matrix.py
+++ src/data2Matrix.py
@@ -1,33 +1,68 @@
+### This file contains a function to transform a route into a matrix ###
+
 import json
 
-# Read and parse standard routes
-with open("./data/standard.json", "r") as standard_file:
-    standard_routes = json.load(standard_file)
-
-# Read and parse actual routes
-with open("./data/actual.json", "r") as actual_file:
-    actual_routes = json.load(actual_file)
-
-# Read and parse drivers
-with open("./data/drivers.json", "r") as drivers_file:
-    drivers = json.load(drivers_file)
-
-# Read and parse cities
+# read and parse cities.json
 with open("./data/cities.json", "r") as cities_file:
     cities = json.load(cities_file)
+# cities = ["Trento", "Roma", "Venizia"]
 
-# Read and parse merchTypes
+# read and parse merchTypes.json
 with open("./data/merchTypes.json", "r") as merchTypes_file:
-    merchTypes = json.load(merchTypes_file)
+    merchandise_types = json.load(merchTypes_file)
 
-def routeToMatrix(route, elts):
+
+def pairs_of_cities(cities):
     '''
-    Convert a route to a matrix
+    Return a list of pairs of cities
+    cities : List[str]
+    '''
+    pairs = []
+    for i in range(len(cities)):
+        for j in range(i + 1, len(cities)):
+            pairs.append((cities[i], cities[j]))
+    return pairs
+
+
+def route_to_matrix(route):
+    '''
+    Return a matrix of a given route, with the number of merchandise transported
     route : List[Dict[str, Any]]
-    elts : List[Any]
-    routeToMatrix(route, elts) -> List[List[int]]
     '''
-    matrix = []
+    pairs_from_route = []
+    merch_types_from_route = []
+    
     for step in route:
-        matrix.append([1 if elt in step["merchandise"] else 0 for elt in elts])
+        pair = (step["from"], step["to"])
+        pairs_from_route.append(pair)
+        for merch_type in step["merchandise"]:
+            merch_types_from_route.append(merch_type)
+
+    pairs_from_route = list(set(pairs_from_route))
+    merch_types_from_route = list(set(merch_types_from_route))
+
+    matrix = [[0 for i in range(len(merchandise_types))] for j in range(len(pairs_of_cities(cities)))]
+    for step in route:
+        for merch_type in step["merchandise"]:
+            matrix[pairs_from_route.index((step["from"], step["to"]))][merch_types_from_route.index(merch_type)] = step["merchandise"][merch_type]
+
     return matrix
+
+
+
+
+
+# # for tests
+
+# with open("./data/standard.json", "r") as standard_file:
+#     standard_routes = json.load(standard_file)
+
+# routes = []
+# for route in standard_routes:
+#     routes.append(route["route"])
+
+# pairs = pairs_of_cities(cities)
+# merch_types = merchandise_types
+# matrix = route_to_matrix(routes[0])
+
+# print(matrix)
