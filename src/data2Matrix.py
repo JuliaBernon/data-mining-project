1995e352a110a74a76fd4e82076aeaaac4babffd
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2023-11-09 21:34:36 +0100

    sparsing matrix

--- src/data2Matrix.py
+++ src/data2Matrix.py
@@ -1,6 +1,7 @@
-### This file contains a function to transform a route into a matrix ###
+### This file contains a function to transform one route into a matrix ###
 
 import json
+import scipy.sparse as sp
 
 # read and parse cities.json
 with open("./data/cities.json", "r") as cities_file:
@@ -49,20 +50,55 @@ def route_to_matrix(route):
     return matrix
 
 
+# sparse matrix
+# def route_to_sparse_matrix(route):
+#     '''
+#     Return a sparse matrix of a given route, with the number of merchandise transported
+#     route : List[Dict[str, Any]]
+#     '''
+#     pairs_from_route = []
+#     merch_types_from_route = []
+    
+#     for step in route:
+#         pair = (step["from"], step["to"])
+#         pairs_from_route.append(pair)
+#         for merch_type in step["merchandise"]:
+#             merch_types_from_route.append(merch_type)
+
+#     pairs_from_route = list(set(pairs_from_route))
+#     merch_types_from_route = list(set(merch_types_from_route))
+
+#     matrix = sp.lil_matrix((len(pairs_of_cities(cities)), len(merchandise_types)), dtype=int)
+#     for step in route:
+#         for merch_type in step["merchandise"]:
+#             matrix[pairs_from_route.index((step["from"], step["to"])), merch_types_from_route.index(merch_type)] = step["merchandise"][merch_type]
+
+#     return matrix
+
+def matrix_sparse(matrix):
+    '''
+    Return a sparse matrix of a given matrix
+    matrix : List[List[int]]
+    '''
+    matrix = sp.lil_matrix(matrix, dtype=int)
+    return matrix
 
 
+# for tests
 
-# # for tests
+with open("./data/standard.json", "r") as standard_file:
+    standard_routes = json.load(standard_file)
 
-# with open("./data/standard.json", "r") as standard_file:
-#     standard_routes = json.load(standard_file)
+routes = []
+for route in standard_routes:
+    routes.append(route["route"])
 
-# routes = []
-# for route in standard_routes:
-#     routes.append(route["route"])
+pairs = pairs_of_cities(cities)
+merch_types = merchandise_types
 
-# pairs = pairs_of_cities(cities)
-# merch_types = merchandise_types
-# matrix = route_to_matrix(routes[0])
+# transform the first route of the standard_routes into a matrix
+matrix = route_to_matrix(routes[0])
+matrix2 = matrix_sparse(matrix)
 
-# print(matrix)
+print(matrix)
+print(matrix2)
