61a4345b40f4c0a6112e4dd62b9069465e2c6f82
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2023-11-10 14:42:53 +0100

    drivers generation

--- src/dataGenerator/driverGenerator.py
+++ src/dataGenerator/driverGenerator.py
@@ -1,3 +1,50 @@
 ### This file contains scripts to create driver agenda ###
 
-import json
+# drivers.json
+# { “id” : “A”,
+# 	“pref_cities” : {“a” : proba_a, “b” : proba_b ,...},
+# 	“pref_merch” : {“merch1” : proba1, “merch2” : proba2,...},
+# 	“tendancy” : {“merch1” : 1, “merch2”: -1, …},
+# 	“amp_tendancy” : 5
+# },
+
+
+# function to generate a driver
+
+import random
+
+def set_tendancy(pref_merch):
+    '''
+    Return a dictionary of merchandise types and the driver's tendancy to get more or less of each type
+    
+    pref_merch : Dict[str, float]
+
+    set_tendancy(pref_merch) -> Dict[str, int]
+    '''
+    merch_tendancy = {}
+    for merch in pref_merch:
+        if pref_merch[merch] <= 0.5:
+            merch_tendancy[merch] = -1
+        elif pref_merch[merch] > 0.5 and pref_merch[merch] <= 0.8:
+            merch_tendancy[merch] = 0
+        else:
+            merch_tendancy[merch] = 1
+    return merch_tendancy
+
+def generate_driver(cities, merchandise_types):
+
+    '''
+    Generate a driver, given a set of cities and merchandise types
+    
+    cities : List[str]
+    merchandise_types : List[str]
+
+    generate_driver(cities, merchandise_types) -> Dict[str, Any]
+    '''
+
+    driver = {}
+    driver["pref_cities"] = {city: random.random() for city in cities}
+    driver["pref_merch"] = {item: random.random() for item in merchandise_types}
+    driver["tendancy"] = set_tendancy(driver["pref_merch"])
+    driver["amp_tendancy"] = random.randint(1, 10)
+    return driver
--- src/driverGeneration.py
+++ src/driverGeneration.py
@@ -1,1 +1,22 @@
 ### This file creates drivers.json according to drivers' agenda ###
+
+import json
+from dataGenerator.driverGenerator import generate_driver
+
+# read and parse cities.json
+with open("./data/cities.json", "r") as cities_file:
+    cities = json.load(cities_file)
+
+# read and parse merchTypes.json
+with open("./data/merchTypes.json", "r") as merchTypes_file:
+    merchandise_types = json.load(merchTypes_file)
+
+## generate drivers.json
+nb_drivers = 20 # define the number of drivers
+drivers = []
+for i in range(nb_drivers):
+    drivers.append({"id": f"d{i + 1}", "agenda": generate_driver(cities, merchandise_types)})
+
+# save data into drivers.json
+with open("./data/drivers.json", "w") as drivers_file:
+    json.dump(drivers, drivers_file, indent=4)
