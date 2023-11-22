32e64320d048e5002583b22f785f0fb9cf634309
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2023-11-22 18:47:24 +0100

    Update exe_script.py

--- src/exe_script.py
+++ src/exe_script.py
@@ -1,13 +1,18 @@
 import os
+# run : python3 exe_script.py
 
-def run_script(script):
+def run_script(script,arg):
     try:
-        os.system(f"python {script}")
+        os.system(f"python {script} {arg}")
     except Exception as e:
         print(f"Error while running {script}: {e}")
 
 # Generate drivers, standard routes, and actual routes
-scripts = ["./src/driverGeneration.py", "./src/stdGeneration.py", "./src/actualGeneration.py"]
-for script in scripts:
-    run_script(script)
+scripts = [
+    ("./src/driverGeneration.py", 100),
+    ("./src/stdGeneration.py", 1000),
+    ("./src/actualGeneration.py", 5000)
+]
 
+for script, arg in scripts:
+    run_script(script,arg)
