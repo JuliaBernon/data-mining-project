f333f26a2b0c796ac222523f360ec136e4d6f20b
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2023-12-15 16:17:32 +0100

    Update exe_script.py

--- src/exe_script.py
+++ src/exe_script.py
@@ -9,6 +9,7 @@ def run_script(script, *args):
 
 # Generate drivers, standard routes, and actual routes
 scripts = [
+<<<<<<< Updated upstream
     ("./src/mainRulesGeneration.py",""),
     ("./src/driverGeneration.py", 100), # see number by default in each script
     ("./src/stdGeneration.py", 500),
@@ -19,34 +20,28 @@ scripts = [
     ("./src/rankings.py", ""),# takes a bit of time (~1min)
     ("./src/question_2.py", ""),# takes a lot of time (at least 10 secs per driver)
     ("./src/q2_test.py", ""),# takes a lot of time (at least 10 secs per driver)
+=======
+    # # ("./src/mainRulesGeneration.py",""),
+    # # ("./src/driverGeneration.py", 100), # see number by default in each script
+    
+    # ("./src/stdGeneration.py", 1000),
+
+    ("./src/actualGeneration.py", 16000, "./data/standard.json", "./data/actual3_16000.json"),
+    ("./src/merchFIAndAssoRules.py", 0.3, 0.8, "./data/actual3_16000.json", "./data/csv/freq_items3_16000.csv", "./data/csv/asso_rules3_16000.csv", "./data/freq_items3_16000.json"),
+    ("./src/recStandard.py", 1000, "./data/csv/freq_items3_16000.csv", "./results/recStandard3_16000.json"),
+    ("./src/actualGeneration.py", 16000, "./results/recStandard3_16000.json", "./data/newActual3_16000.json")
+    
+    # # ("./src/rankings.py", ""),# takes a bit of time (~1min)
+    # # ("./src/question_2.py", ""),# takes a lot of time (at least 10 secs per driver)
+    # # ("./src/q2_test.py", ""),# takes a lot of time (at least 10 secs per driver)
+>>>>>>> Stashed changes
 ]
 for script_args in scripts:
     run_script(*script_args)
 
-# actual : 1000, threshold : 0.75, recStandard : 500, newActual : 1000
-# actual1 : 1000, threshold : 0.8, recStandard : 500, newActual : 1000
-# actual2 : 2000, threshold : 0.8, recStandard : 500, newActual : 2000
-# actual3 : 3000, threshold : 0.8, recStandard : 500, newActual : 3000
-# actual4 : 10000, threshold : 0.75, recStandard : 500, newActual : 10000
-# actual5 : 10000, threshold : 0.75, recStandard : 500, newActual : 10000
 
-# actual6 : 1000, threshold : 0.75, recStandard : 500, newActual : 1000
-# actual7 : 1000, threshold : 0.75, recStandard : 500, newActual : 1000
-# actual8 : 1000, threshold : 0.75, recStandard : 500, newActual : 1000
-# actual9 : 1000, threshold : 0.75, recStandard : 500, newActual : 1000
-
-# actual10 : 2000, threshold : 0.8, recStandard : 500, newActual : 2000
-# actual11 : 2000, threshold : 0.8, recStandard : 500, newActual : 2000
-# actual12 : 2000, threshold : 0.8, recStandard : 500, newActual : 2000
-# actual13 : 2000, threshold : 0.8, recStandard : 500, newActual : 2000
-
-# actual14 : 3000, threshold : 0.8, recStandard : 500, newActual : 3000
-# actual15 : 3000, threshold : 0.8, recStandard : 500, newActual : 3000
-# actual16 : 3000, threshold : 0.8, recStandard : 500, newActual : 3000
-# actual17 : 3000, threshold : 0.8, recStandard : 500, newActual : 3000
-
-# actual18 : 10000, threshold : 0.75, recStandard : 500, newActual : 10000
-# actual19 : 10000, threshold : 0.75, recStandard : 500, newActual : 10000
-# actual20 : 10000, threshold : 0.75, recStandard : 500, newActual : 10000
-# actual21 : 10000, threshold : 0.75, recStandard : 500, newActual : 10000
-
+## for all the tests : threshold = 0.8, support = 0.3, max_len = 3
+## for 100 standard routes : 
+    # xxx100 : 100 actual, 100 new actual, 100 rec standard
+## for 500 standard routes : 
+    # xxx500 : 500 actual, 500 new actual, 500 rec standard
