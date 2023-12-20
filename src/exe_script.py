e220266bb62ea113101e3692eb57cee9af036781
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2023-12-20 11:06:42 +0100

    Update exe_script.py

--- src/exe_script.py
+++ src/exe_script.py
@@ -8,15 +8,22 @@ def run_script(script, *args):
         print(f"Error while running {script}: {e}")
 
 # Generate drivers, standard routes, and actual routes
-scripts = [
-    ("./src/mainRulesGeneration.py",""),
-    ("./src/driverGeneration.py", 100), # see number by default in each script
-    ("./src/stdGeneration.py", 500),
 
-    ("./src/actualGeneration.py", 10000, "./data/standard.json", "./data/actual.json"),
-    #("./src/merchFIAndAssoRules.py", 0.3, 0.8, "./data/actual.json", "./data/csv/freq_items.csv", "./data/csv/asso_rules.csv", "./data/freq_items.json"),
-    #("./src/recStandard.py", 500, "./data/csv/freq_items.csv", "./results/recStandard.json"),
-    #("./src/actualGeneration.py", 1000, "./results/recStandard.json", "./data/newActual.json")
+stdid = 1000
+nbactual = 8000
+fileid = f"{stdid}_{nbactual}"
+
+
+scripts = [
+    # ("./src/mainRulesGeneration.py",""),
+    # ("./src/driverGeneration.py", 100), # see number by default in each script
+    
+    # ("./src/stdGeneration.py", 500),
+
+    ("./src/actualGeneration.py", nbactual, f"./data/standard{stdid}.json", f"./data/actual{fileid}.json"),
+    ("./src/merchFIAndAssoRules.py", 0.3, 0.8, f"./data/actual{fileid}.json", f"./data/csv/freq_items{fileid}.csv", f"./data/csv/asso_rules{fileid}.csv", f"./data/freq_items{fileid}.json"),
+    ("./src/recStandard.py", stdid, f"./data/csv/freq_items{fileid}.csv", f"./results/recStandard{fileid}.json"),
+    ("./src/actualGeneration.py", nbactual, f"./results/recStandard{fileid}.json", f"./data/newActual{fileid}.json")
     
     #("./src/rankings.py", ""),# takes a bit of time (~1min)
     #("./src/question_2.py", ""),# takes a lot of time (at least 10 secs per driver)
