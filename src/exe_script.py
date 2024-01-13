f7618f5a086a07be27dfaeba4438963dbc5ed9c3
Julia Bernon <bernon1u@etu.univ-lorraine.fr>
2024-01-13 12:50:44 +0100

    Update exe_script.py

--- src/exe_script.py
+++ src/exe_script.py
@@ -18,17 +18,9 @@ fileid = ""
 scripts = [
     ("./src/mainRulesGeneration.py",""),
     ("./src/driverGeneration.py", 100), # see number by default in each script
-    
     ("./src/stdGeneration.py", stdid),
-
     ("./src/actualGeneration.py", nbactual, f"./data/standard{stdid}.json", f"./data/actual{fileid}.json"),
     ("./src/merchFIAndAssoRules.py", 0.3, 0.8, f"./data/actual{fileid}.json", f"./data/csv/freq_items{fileid}.csv", f"./data/csv/asso_rules{fileid}.csv", f"./data/freq_items{fileid}.json"),
-    ("./src/recStandard.py", stdid, f"./data/csv/freq_items{fileid}.csv", f"./results/recStandard{fileid}.json"),
-    ("./src/actualGeneration.py", nbactual, f"./results/recStandard{fileid}.json", f"./data/newActual{fileid}.json")
-    
-    #("./src/rankings.py", ""),# takes a bit of time (~1min)
-    #("./src/question_2.py", ""),# takes a lot of time (at least 10 secs per driver)
-    #("./src/q2_test.py", ""),# takes a lot of time (at least 10 secs per driver)
 ]
 for script_args in scripts:
     run_script(*script_args)
